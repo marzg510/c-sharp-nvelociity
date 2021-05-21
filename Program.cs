77302df92f65ff5a76e0a88a3cb3addeea932a74
Masaru Gotou <masaru.gotou@gmail.com>
2021-05-21 21:03:00 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Diagnostics;
+using System.Text;
+using System;
+
+namespace c_sharp_nvelocity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 初期処理
+            NVelocity.App.Velocity.Init();
+            // コンテキストを生成する
+            NVelocity.VelocityContext context = new NVelocity.VelocityContext();
+            // 変数と値の組み合わせを格納する
+            context.Put("name", "hoge");
+            // テンプレートファイルを指定してテキストを生成する
+            StringBuilder sb = new StringBuilder();
+            using (StringWriter writer = new StringWriter(sb))
+            {
+                // NVelocity.App.Velocity.MergeTemplate("test.vm", "utf-8", context, writer);
+                NVelocity.App.Velocity.MergeTemplate("test.xml.vm", "utf-8", context, writer);
+            }
+            Debug.WriteLine(sb.ToString());
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+
+
+- https://qiita.com/mxProject/items/ca13e69968036853595d
+
+```
+dotnet add package NVelocity --version 1.2.0
+```
